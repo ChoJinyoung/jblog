add2ffc2c930354fbf866edeb2c1dc5325301d62
soojinchoi <love2001s@naver.com>
2015-11-09 17:29:52 +0900

    다시시작하는거야

--- src/main/java/com/bit2015/jblog/controller/BlogController.java
+++ src/main/java/com/bit2015/jblog/controller/BlogController.java
@@ -6,27 +6,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/blog")
 public class BlogController {
-	
-	
-	@RequestMapping("")
-	public String main(){
-		return "/blog/main";
-	}
-	
-	@RequestMapping("/maindetail")
-	public String maindetail(){
-		return "/blog/main-detail";
-	}
-	@RequestMapping("/writeform")
-	public String writeform(){
-		return "/blog/write";
-	}
-	@RequestMapping("/basic")
-	public String basic(){
-		return "/blog/basic";
-	}
-	@RequestMapping("/category")
-	public String category(){
-		return "/blog/category";
-	}
+
+   @RequestMapping("")
+   public String main(){
+      return "/blog/main";
+   }
+   
+   @RequestMapping("/maindetail")
+   public String maindetail(){
+      return "/blog/main-detail";
+   }
+   @RequestMapping("/writeform")
+   public String writeform(){
+      return "/blog/write";
+   }
+   @RequestMapping("/basic")
+   public String basic(){
+      return "/blog/basic";
+   }
+   @RequestMapping("/category")
+   public String category(){
+      return "/blog/category";
+   }
 }
