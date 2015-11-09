aa16920d25308be8c1acf07fde7403641d05bd0b
soojinchoi <love2001s@naver.com>
2015-11-09 17:19:07 +0900

    롸

---
+++ src/main/java/com/bit2015/jblog/vo/BlogVo.java
@@ -0,0 +1,10 @@
+package com.bit2015.jblog.vo;
+
+public class BlogVo {
+	private long blogNo;
+	private String title;
+	private String tag;
+	private long cntDisplayPost;
+	private String status;
+	private String fileName;
+}
