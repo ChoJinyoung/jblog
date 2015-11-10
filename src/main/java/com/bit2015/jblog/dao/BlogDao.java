774806971733dee2d345cf6a3fec67c003e6a79b
soojinchoi <love2001s@naver.com>
2015-11-10 13:16:06 +0900

    blogVo

---
+++ src/main/java/com/bit2015/jblog/dao/BlogDao.java
@@ -0,0 +1,21 @@
+package com.bit2015.jblog.dao;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.bit2015.jblog.vo.BlogVo;
+
+@Repository
+public class BlogDao {
+	
+	@Autowired
+	SqlSession sqlsession;
+	
+	public BlogVo select(BlogVo vo){
+		BlogVo blogVo = sqlsession.selectOne("blog.blogInfo", vo);
+		System.out.println(vo);
+		return blogVo;
+	}
+	
+}
---
+++ src/main/java/com/bit2015/jblog/vo/BlogVo.java
@@ -0,0 +1,56 @@
+package com.bit2015.jblog.vo;
+
+public class BlogVo {
+
+	private long blogNo;
+	private String title;
+	private String tag;
+	private long cntDisplayPost;
+	private String status;
+	private String fileName;
+	
+	public long getBlogNo() {
+		return blogNo;
+	}
+	public void setBlogNo(long blogNo) {
+		this.blogNo = blogNo;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getTag() {
+		return tag;
+	}
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+	public long getCntDisplayPost() {
+		return cntDisplayPost;
+	}
+	public void setCntDisplayPost(long cntDisplayPost) {
+		this.cntDisplayPost = cntDisplayPost;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	
+	@Override
+	public String toString() {
+		return "BlogVo [blogNo=" + blogNo + ", title=" + title + ", tag=" + tag
+				+ ", cntDisplayPost=" + cntDisplayPost + ", status=" + status
+				+ ", fileName=" + fileName + "]";
+	}
+
+}
